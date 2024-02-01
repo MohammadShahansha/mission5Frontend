d0e1f89022b2578490da614fe1ce6ff568c4deac
MohammadShahansha <shahansha.cou.ict11@gmail.com>
2024-02-02 00:44:32 +0600

    added redux setup

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,8 +1,32 @@
+import { Button } from "antd";
+import { useForm } from "react-hook-form";
+import { useLoginMutation } from "../redux/features/auth/authApi";
+
 const Login = () => {
+  const { register, handleSubmit } = useForm();
+  const [login, { data, error }] = useLoginMutation();
+  console.log("data =>", data);
+  console.log("error =>", error);
+  const onSubmit = (data) => {
+    console.log(data);
+    const userInfo = {
+      userName: data.userName,
+      password: data.password,
+    };
+    login(userInfo);
+  };
   return (
-    <div>
-      <h1>here are login related work</h1>
-    </div>
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <div>
+        <label htmlFor="userName">User Name</label>
+        <input type="text" id="userName" {...register("userName")} />
+      </div>
+      <div>
+        <label htmlFor="password">Password</label>
+        <input type="text" id="password" {...register("password")} />
+      </div>
+      <Button htmlType="submit">Login</Button>
+    </form>
   );
 };
 
